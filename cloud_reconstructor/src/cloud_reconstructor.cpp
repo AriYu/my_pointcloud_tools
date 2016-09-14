2c0d7fe7bf071d6b85840cf0ca33f5a36fbbc531
Arita Yuta <k104009y@mail.kyutech.jp>
2016-09-14 22:17:03 +0900

    add cloud_reconstructor

---
+++ cloud_reconstructor/src/cloud_reconstructor.cpp
@@ -0,0 +1,63 @@
+#include <pcl/visualization/cloud_viewer.h>
+#include <iostream>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/obj_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/surface/gp3.h>
+#include <boost/program_options.hpp>
+#include <pcl/surface/mls.h>
+
+int main (int argc, char** argv)
+{
+  boost::program_options::options_description desc("Options");
+  desc.add_options()
+    ("help", "Print help message")
+    ("pcd_filename", boost::program_options::value<std::string>()->required(), "pcl pcd filename");
+
+  boost::program_options::variables_map vm;
+  try {
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+    if( vm.count("help") ){
+      std::cout << "Basic Command Line Parameter App" << std::endl;
+      std::cerr << desc << std::endl;
+      return 0;
+    }
+    boost::program_options::notify(vm);
+  } catch (boost::program_options::error& e) {
+    std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+    std::cerr << desc << std::endl;
+    return -1;
+  }
+
+  std::string pcd_fname = vm["pcd_filename"].as<std::string>();
+  
+  // Load input file into a PointCloud<T> with an appropriate type
+  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBA> ());
+  // Load bun0.pcd -- should be available with the PCL archive in test 
+  pcl::io::loadPCDFile (pcd_fname, *cloud);
+
+  // Create a KD-Tree
+  pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGBA>);
+
+  // Output has the PointNormal type in order to store the normals calculated by MLS
+  pcl::PointCloud<pcl::PointNormal> mls_points;
+
+  // Init object (second point type is for the normals, even if unused)
+  pcl::MovingLeastSquares<pcl::PointXYZRGBA, pcl::PointNormal> mls;
+ 
+  mls.setComputeNormals (true);
+
+  // Set parameters
+  mls.setInputCloud (cloud);
+  mls.setPolynomialFit (true);
+  mls.setSearchMethod (tree);
+  mls.setSearchRadius (0.1);
+
+  // Reconstruct
+  mls.process (mls_points);
+
+  // Save output
+  pcl::io::savePCDFile ("bun0-mls.pcd", mls_points);
+}
